6e783d33c407a015c96cbb615f99e2c3b12d665c
yuloren <yuloren@d79f43b9-3934-0410-9f86-ab867f5afe81>
2007-07-02 03:13:14 +0000

    Added FLICKR_API_KEY and FLICKR_API_SECRET strings.

--- trunk/PhotoMosaic/Default.aspx.cs
+++ trunk/PhotoMosaic/Default.aspx.cs
@@ -9,9 +9,13 @@ using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
 
 using System.Drawing;
+using FlickrNet;
 
 public partial class _Default : System.Web.UI.Page 
 {
+    private static string FLICKR_API_KEY = "76689f3376d2752abacb6bac4c12f580";
+    private static string FLICKR_API_SECRET = "a095ecfb543abc2d";
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
@@ -19,6 +23,8 @@ public partial class _Default : System.Web.UI.Page
 
     protected void AssembleButton_Click(object sender, EventArgs e)
     {
+        Flickr flickr = new Flickr(FLICKR_API_KEY, FLICKR_API_SECRET);
+
         Assembler assembler = new Assembler();
         ImageMap m = new ImageMap();
         //Objective objective = new Objective(new TargetImage(new Bitmap()));
