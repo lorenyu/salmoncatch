d16b0fa48bc7f43ba8e322821987c0e697883eb3
mfischer1000 <mfischer1000@d79f43b9-3934-0410-9f86-ab867f5afe81>
2007-07-04 06:42:31 +0000

    FinishedImage class partial stub.  This class is the final image produced by the many component images.

---
+++ trunk/PhotoMosaic/App_Code/FinishedImage.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+/// <summary>
+/// Summary description for FinishedImage
+/// </summary>
+public class FinishedImage
+{
+	public FinishedImage(int height, int width)
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    internal void addImage(ComponentImage componentImage)
+    {
+        throw new Exception("The method or operation is not implemented.");
+    }
+}
