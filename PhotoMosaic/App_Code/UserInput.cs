350c24d3131148cfc1eea2e08f82ba624238e768
yuloren <yuloren@d79f43b9-3934-0410-9f86-ab867f5afe81>
2007-07-10 06:22:18 +0000

    userinput handling

--- PhotoMosaic/App_Code/UserInput.cs
+++ PhotoMosaic/App_Code/UserInput.cs
@@ -15,4 +15,6 @@ public class UserInput
 {
     public string targetImageFilename;
     public string componentImageDirectory;
+    public int numHorizontalImages;
+    public int numVerticalImages;
 }
