932402cb089f02d15662b94121d0eb3407cb6632
mfischer1000 <mfischer1000@d79f43b9-3934-0410-9f86-ab867f5afe81>
2007-08-18 08:50:28 +0000

    download all photo pages

--- trunk/PhotoMosaic/App_Code/FlickrUtil.cs
+++ trunk/PhotoMosaic/App_Code/FlickrUtil.cs
@@ -15,37 +15,57 @@ public class FlickrUtil
 {
     private static string FLICKR_API_KEY = "76689f3376d2752abacb6bac4c12f580";
     private static string FLICKR_API_SECRET = "a095ecfb543abc2d";
+    private static int NUMBER_PHOTOS_PER_PAGE = 100;
+    private static int MAX_PAGES = 1;
 
-	public FlickrUtil()
-	{
-	}
-
-    public Flickr NewFlickr()
+    public static Flickr NewFlickr()
     {
-        return new Flickr(FLICKR_API_KEY);
+        return new Flickr(FLICKR_API_KEY, FLICKR_API_SECRET);
     }
 
-    public string GetUserID(UserInput userInput)
+    public static string GetUserID(UserInput userInput)
     {
         FoundUser user = userInput.flickr.PeopleFindByUsername(userInput.userName);
         return user.UserId;
     }
 
-    public void getAllPublicPhotos(UserInput userInput)
-    {
-        Photos photos = userInput.flickr.PeopleGetPublicPhotos(userInput.userID);
-        savePhotos(photos);
-    }
-
-    public void search(string search, UserInput userInput)
+    public static void GetAllPublicPhotos(UserInput userInput)
     {
         PhotoSearchOptions searchOptions = new PhotoSearchOptions();
-        searchOptions.Tags = search;
-        Photos photos = userInput.flickr.PhotosSearch(searchOptions);
-        savePhotos(photos);
+        searchOptions.UserId = userInput.userID;
+        PhotoCollection collection = Search(userInput, searchOptions);
+        savePhotos(collection);
     }
 
-    public void authenticateStep1(UserInput userInput)
+    public static void SearchByTag(UserInput userInput, string tag)
+    {
+        PhotoSearchOptions searchOptions = new PhotoSearchOptions();
+        searchOptions.Tags = tag;
+        PhotoCollection collection = Search(userInput, searchOptions);
+        savePhotos(collection);
+    }
+
+    private static PhotoCollection Search(UserInput userInput, PhotoSearchOptions searchOptions)
+    {
+        searchOptions.PerPage = NUMBER_PHOTOS_PER_PAGE;
+        Photos photos = userInput.flickr.PhotosSearch(searchOptions);
+        PhotoCollection mergedCollection = new PhotoCollection();
+        int pages = (int)photos.TotalPages;
+        if (pages > MAX_PAGES)
+        {
+            pages = MAX_PAGES;
+        }
+        for (int i = 0; i < pages; i++)
+        {
+            searchOptions.Page = i;
+            photos = userInput.flickr.PhotosSearch(searchOptions);
+            mergedCollection.AddRange(photos.PhotoCollection);
+        }
+        return mergedCollection;
+    }
+
+
+    public static void authenticateStep1(UserInput userInput)
     {
         userInput.flickr = new Flickr(FLICKR_API_KEY, FLICKR_API_SECRET);
         userInput.tempFrob = userInput.flickr.AuthGetFrob();
@@ -53,7 +73,7 @@ public class FlickrUtil
         System.Diagnostics.Process.Start(flickrUrl);
     }
 
-    public void authenticateStep2(UserInput userInput)
+    public static void authenticateStep2(UserInput userInput)
     {
         userInput.flickr = new Flickr(FLICKR_API_KEY, FLICKR_API_SECRET);
         try
@@ -66,10 +86,10 @@ public class FlickrUtil
         }
     }
 
-    private void savePhotos(Photos photos)
+    private static void savePhotos(PhotoCollection allPhotos)
     {
-        PhotoCollection allPhotos = photos.PhotoCollection;
         int i = 0;
+        
         foreach (Photo photo in allPhotos)
         {
             string dest = Settings.USER_DIR + @"\img" + i + ".png";
@@ -78,7 +98,7 @@ public class FlickrUtil
         }
     }
 
-    public Image GetImageFromURL(string strURL)
+    public static Image GetImageFromURL(string strURL)
     {
         Image retVal = null;
         try
