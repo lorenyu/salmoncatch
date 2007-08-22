1d49550696c2ece54bb9dff2e0763b91c2ecb8d5
mfischer1000 <mfischer1000@d79f43b9-3934-0410-9f86-ab867f5afe81>
2007-08-22 05:17:16 +0000

    added new DownloadImageThread

---
+++ trunk/PhotoMosaic/App_Code/DownloadImageThread.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Drawing;
+using System.Collections.Generic;
+using FlickrNet;
+using System.IO;
+
+/// <summary>
+/// Summary description for DownloadImageThread
+/// </summary>
+public class DownloadImageThread
+{
+    private Photo[] photos;
+    private List<Bitmap> bitmaps;
+
+	public DownloadImageThread(Photo[] photos)
+	{
+        this.photos = photos;
+	}
+
+    public void BeginDownload()
+    {
+        bitmaps = new List<Bitmap>();
+        foreach (Photo photo in photos)
+        {
+            Bitmap bitmap = WebUtil.GetBitmap(photo.ThumbnailUrl);
+            bitmaps.Add(bitmap);
+        }
+    }
+
+    public List<Bitmap> GetResult()
+    {
+        return bitmaps;
+    }
+}
